dde108ecb0905dcc2dd7093b7f71cef0896579ba
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-09-19 18:10:36 -0500

    Switch to React, and render sorted repos.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
 
 
-var github = new Github({
-  // username: "YOU_USER",
-  // password: "YOUR_PASSWORD",
-  // auth: "basic"
-});
+var withRepos = function (repos) {
 
-var user = github.getUser();
+  repos = _.sortBy(repos, function (i) {
+    return i.name.toLowerCase();
+  });
 
-user.userRepos('AndersDJohnson', function(err, repos) {
-	console.log(arguments);
-});
+  console.log(repos);
+
+  var reposCmp = [];
+
+  if (repos) {
+    repos.forEach(function (repo) {
+      reposCmp.push(
+        <div>
+          <a href={repo.html_url}>{repo.name}</a>
+        </div>
+      );
+    })
+  }
+
+  React.render(
+    <div>{reposCmp}</div>
+    ,
+    document.getElementById('wrap')
+  );
+
+};
+
+
+var repos = localStorage.getItem('repos');
+
+if (repos) {
+  repos = JSON.parse(repos);
+  withRepos(repos);
+}
+else {
+
+  var github = new Github({
+    // username: "YOU_USER",
+    // password: "YOUR_PASSWORD",
+    // auth: "basic"
+  });
+
+  var user = github.getUser();
+
+  user.userRepos('AndersDJohnson', function(err, repos) {
+    if (err) throw err;
+    localStorage.setItem('repos', JSON.stringify(repos));
+    withRepos(repos);
+  });
+}
+
